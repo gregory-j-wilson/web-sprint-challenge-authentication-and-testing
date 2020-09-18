9bc0c366cde9cd56c5f67f1d09362291585b8e75
Gregory Wilson <gjwilson7390@gmail.com>
2020-09-18 10:26:18 -0500

    got files connected, model created, and working on register post request

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,46 @@
 const router = require('express').Router();
 
+const bcryptjs = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const Users = require("../database/users-model");
+
+const restricted = require("./authenticate-middleware");
+
+
 router.post('/register', (req, res) => {
-  // implement registration
+  
+  const credentials = req.body
+
+  if (isValid(credentials)) {
+
+    const rounds = process.env.BCRYPT_ROUNDS || 8
+    const hash = bcryptjs.hashSync(credentials.password, rounds)
+
+    credentials.password = hash
+
+    Users.add(credentials)
+        .then(user => {
+          res.status(201).json({data: user})
+        })
+
+
+
+  }
+
 });
 
 router.post('/login', (req, res) => {
   // implement login
 });
+
+
+//-----------------------------------------
+
+
+
+function isValid(user) {
+  return Boolean(user.username && user.password && typeof user.password === "string");
+}
 
 module.exports = router;
